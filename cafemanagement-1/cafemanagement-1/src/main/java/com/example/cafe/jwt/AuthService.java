a347add12dc3b96bfb012767ca3aeee5496ca8d4
Mit1506 <mitvekariya123@gmail.com>
2025-01-21 14:29:09 +0530

    Delete cafemanagement-1 directory

--- cafemanagement-1/cafemanagement-1/src/main/java/com/example/cafe/jwt/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.example.cafe.jwt;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface AuthService {
-	
-	 UserDetailsService userDetailsService();
-
-}
